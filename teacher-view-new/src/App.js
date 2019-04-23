c6a0448c08d470c5dec08ecdc6a88b43d46f83d6
teropaj <teropaj@gmail.com>
2019-04-23 14:32:34 +0300

    json imported

--- teacher-view-new/src/App.js
+++ teacher-view-new/src/App.js
@@ -1,11 +1,24 @@
+'use strict'
+
+
 import React, { Component } from 'react';
  
 import './App.css';
 import Search from './components/Search'
+import { strict } from 'assert';
+import postData from './components/teacherViewJson.json'
+ 
 
 class App extends Component {
- 
-  
+
+
+componentDidMount(){
+  console.log(postData)
+} 
+
+
+
+
   
   render() {
      
